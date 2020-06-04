3637922cfe08fc194205284ec92afe0b4c45bdcc
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-04 19:03:57 +0900

    addLogInPage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,10 +15,6 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    # added_attrs = [ :name, :email, :password, :password_confirmation　]
-    # devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    # devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    # devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
 
 end
