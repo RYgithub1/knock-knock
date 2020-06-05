c84e97dbf5186fda0bc863ebadf9ff36f132ab09
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-05 20:49:25 +0900

    validation_photos_and_language

--- app/models/nation.rb
+++ app/models/nation.rb
@@ -1,5 +1,6 @@
 class Nation < ApplicationRecord
 
+  # to Abouts Table
   validates :name, presence: true
   has_many :abouts, through: :about_nations
   has_many :about_nations, dependent: :destroy
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,8 @@
 class Photo < ApplicationRecord
+
+  # to Abouts Table
+  belongs_to :about
+  validates :about_id, presence: true
+  validates :image, presence: true
+
 end
