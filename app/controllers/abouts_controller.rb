c8696f53bc817413ca07bcf575782c4417d089e1
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-06 11:09:53 +0900

    add_abouts#new_basis

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -1,17 +1,48 @@
 class AboutsController < ApplicationController
 
-  # def index
-  #   # @abouts = About.all
-  # end
-
   def new
-    # @about = About.new
+    @about = About.new
+    # @about.pictures.new
+
+
   end
 
   def create
+    About.create(about_params)
+    @about = About.new(about_params)
+    if @about.save
+      redirect_to about_path(@about)
+      # redirect_to about_path(@about.id)
+    else
+      flash.now[:alert] = "Would you fill in?"
+      # redirect_to new_about_path
+      render :new
+    end
   end
 
+
   def show
+
+
+    @about = About.find_by(id: params[:id])
+    if @about.nil?
+      @abouts = About.all
+      flash.now[:alert] = "Your About was not found"
+      render :new
+    end
+  end
+
+
+
+  private
+  def about_params
+    params.require(:about).permit(
+      :sex,
+      :body,
+      :invitation
+    ).merge(user_id: current_user.id)
+    # ,pictures_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
+
   end
 
 end
