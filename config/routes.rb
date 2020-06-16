c9ba536853d39babd7ec2db900aae6dc16263a38
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-16 19:49:10 +0900

    changedRouting_maps#index

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   root to: "maps#index"
 
-  resources :maps,     only: [:index]
+  resource  :maps,     only: [:index]
   resources :abouts,   except: [:index]
   resources :pairs,    except: [:index, :edit, :update]
   resources :messages, only: [:index, :show]
