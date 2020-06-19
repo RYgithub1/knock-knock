5baffe46274a27e16b1014952c5513e93e58e026
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-19 15:17:04 +0900

    implememntScrolFunction

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,37 +1,5 @@
 $(function () {
-  var xyz1 = function () {
-    var xyz10;
-  };
-
-  function buildHTML(message) {
-    var xyx20;
-  }
-
-  $(".messForm").on("submit", function (e) {
-    e.preventDefault();
-    // this = form facter itself from ".messForm"
-    var formData = new FormData(this);
-    // action="/pairs/pair_id/messages" -> messages#index/create
-    var urlAction = $(this).attr("action");
-
-    $.ajax({
-      type: "POST",
-      url: urlAction,
-      data: formData,
-      dataType: "json",
-      processData: false,
-      contentType: false,
-    })
-      .done(function (data) {
-        // data = res after req to sv
-        var html = buildHTML(data);
-      })
-      .fail(function () {
-        alert("failed to send");
-      });
-  });
-
-  var xyz3 = function () {
-    var xyz30;
-  };
+  var scrollElement = window.document.documentElement;
+  var toBottom = scrollElement.scrollHeight - scrollElement.clientHeight;
+  window.scroll(0, toBottom);
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,29 +7,24 @@ class MessagesController < ApplicationController
     @messages = @pair.messages.includes(:user)
   end
 
-              
   def create
     @pair = Pair.find_by(id: params[:pair_id])
     @message = @pair.messages.new(message_params)
     if @message.save
-      respond_to do |format|
-        format.html {redirect_to pair_messages_path(@pair)}
-        format.json
-        # messages#index needs pair_id
-        # redirect_to pair_messages_path(@pair)
-      end
+      # messages#index needs pair_id
+      redirect_to pair_messages_path(@pair)
     else
       @messages = @pair.messages.includes(:user)
       render :index
     end
   end
-                  
+
 
   private
   def message_params
     # merge two hashes to register user_id for messages table
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-    # user_id テスト用別id
+    # user_id検証
     # params.require(:message).permit(:content, :image).merge(user_id: 6)
   end
 end
