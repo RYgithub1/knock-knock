a0ddb24781b827a1c70cdc7b7fae09887d311dcf
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-20 09:54:36 +0900

    make_model_and_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'hangers/create'
+  get 'hangers/destroy'
   devise_for :users, controllers: {
     sessions: "users/sessions",
     passwords: "users/passwords",
