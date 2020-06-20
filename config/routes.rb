e1b234fc42652a48153c5da927bc3177c446cbaf
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-20 10:02:09 +0900

    add_resource_(without id)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'hangers/create'
-  get 'hangers/destroy'
   devise_for :users, controllers: {
     sessions: "users/sessions",
     passwords: "users/passwords",
@@ -10,7 +8,10 @@ Rails.application.routes.draw do
 
   root to: "maps#index"
   resources :maps, only: [:index]
-  resources :abouts, except: [:index]
+
+  resources :abouts, except: [:index] do
+    resource :hangers, only: [:create, :destroy]
+  end
 
   resources :pairs, except: [:show, :edit, :update] do
     resources :messages, only: [:index, :create]
