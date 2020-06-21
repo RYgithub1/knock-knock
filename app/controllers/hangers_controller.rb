403a1eb3f3d02d42bed7d9015dd51b6889a1e036
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-21 18:25:17 +0900

    before_actionフィルタリング

--- app/controllers/hangers_controller.rb
+++ app/controllers/hangers_controller.rb
@@ -1,18 +1,24 @@
 class HangersController < ApplicationController
 
+  before_action :set_abouts, only: [:create, :destroy]
+
+
   def create
-    @about = About.new(id: params[:about_id])
     hanger = current_user.hangers.new(about_id: params[:about_id])
     hanger.save
     redirect_to about_path(@about.id)
   end
 
   def destroy
-    @about = About.new(id: params[:about_id])
     hanger = Hanger.find_by(about_id: params[:about_id], user_id: current_user.id)
     hanger.destroy
     redirect_to about_path(@about)
   end
 
+
+  private
+  def set_abouts
+    @about = About.new(id: params[:about_id])
+  end
 
 end
