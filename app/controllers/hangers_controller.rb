17baa75d3d703b067dca1f7ad707f1ee12bceb0c
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-21 17:44:08 +0900

    Hangerのdelete後の遷移先調整

--- app/controllers/hangers_controller.rb
+++ app/controllers/hangers_controller.rb
@@ -8,9 +8,10 @@ class HangersController < ApplicationController
   end
 
   def destroy
+    @about = About.new(id: params[:about_id])
     hanger = Hanger.find_by(about_id: params[:about_id], user_id: current_user.id)
     hanger.destroy
-    redirect_to root_path
+    redirect_to about_path(@about)
   end
 
 
