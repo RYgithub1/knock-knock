3f357f0d1cebe4a306f64fb9a0275a7f3bb65fb8
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-22 21:21:57 +0900

    fixStyle

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -4,50 +4,31 @@ class AboutsController < ApplicationController
 
 
   def new
-    # @about = About.find_by(id: params[:id])
     @about = About.new
-    # aboutモデルと紐付くxxxモデルの,インスタンスを作成
+    # aboutモデルに紐付く各モデルのインスタンスを設定
     @about.photos.new
     @about.pictures.new
   end
 
   def create
-    # @about = About.find_by(id: params[:id])
     @about = About.new(about_params)
     if @about.save
-      # redirect_to root_path(@about)
       redirect_to about_path(@about.id)
-      # redirect_to about_path
-      # redirect_to about_path(id: params[:id])
-      # redirect_to about_path(@about)
-      # redirect_to about_path(@about.id) これやろフリマより！
     else
       render :new
-      # redirect_to new_about_path
-      # flash.now[:alert] = "Would you fill in?"
     end
   end
 
-
   def show
     @about = About.find_by(id: params[:id])
-    # @user = @about.user
-    # @tags = @about.tags
     @photos = Photo.where(id: @about.photos.ids)
     @pictures = Picture.where(id: @about.pictures.ids)
-    # if @about.nil?
-    #   flash.now[:alert] = 'Your "About ME" was not found.'
-    #   render :new
-    # end
   end
-
-
 
   def edit
     @about = About.find_by(id: params[:id])
     @photos = Photo.where(id: @about.photos.ids)
     @pictures = Picture.where(id: @about.pictures.ids)
-
   end
 
   def update
@@ -67,7 +48,6 @@ class AboutsController < ApplicationController
   end
 
 
-  # === Strong parameter =======================
   private
   def about_params
     params.require(:about).permit(
