0ac67c3598c32ccfc3ab39307d42caeb9ab4e144
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-22 11:00:29 +0900

    implement_HangerFunction_with_Ajax

--- app/controllers/hangers_controller.rb
+++ app/controllers/hangers_controller.rb
@@ -1,23 +1,29 @@
 class HangersController < ApplicationController
 
-  before_action :set_abouts, only: [:create, :destroy]
+  before_action :set_about, only: [:create, :destroy]
 
 
   def create
     hanger = current_user.hangers.new(about_id: params[:about_id])
     hanger.save
-    redirect_to about_path(@about.id)
+    # redirect_to about_path(@about.id)
+    # render "create.html.haml"
+    # render "ajaxCreateDestroy.js.haml"
   end
 
   def destroy
+    # 以下代替案
+    # hanger = current_user.hangers.find_by(about_id: params[:about_id])
     hanger = Hanger.find_by(about_id: params[:about_id], user_id: current_user.id)
     hanger.destroy
-    redirect_to about_path(@about)
+    # redirect_to about_path(@about)
+    # render "destroy.html.haml"
+    # render "ajaxCreateDestroy.js.haml"
   end
 
 
   private
-  def set_abouts
+  def set_about
     @about = About.new(id: params[:about_id])
   end
 
