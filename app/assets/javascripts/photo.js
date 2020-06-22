256ef693567e47f6058b3d2e8825772bd5a273b9
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-22 16:41:03 +0900

    Pics/PhotosMoveSeparetely__and__onlyOnePhotoForPhotoTable

--- app/assets/javascripts/photo.js
+++ app/assets/javascripts/photo.js
@@ -1,6 +1,6 @@
 $(document).on("turbolinks:load", function () {
-  const buildFileField = function (index) {
-    const html = `<div class="jsFileGroupIcon" data-index="${index}">
+  const buildFileFieldIcon = function (index) {
+    const html = `<div class="jsFileGroupIcon" data-indexIcon="${index}">
               <label class="jsFileLabelIcon" for="about_photos_attributes_${index}_image">
               <input class="jsFileIcon" type="file" name="about[photos_attributes][${index}][image]" id="about_photos_attributes_${index}_image">
               <div class="jsRemoveIcon">delete icon</div>
@@ -8,45 +8,45 @@ $(document).on("turbolinks:load", function () {
     return html;
   };
 
-  const buildImg = function (tIndex, bUrl) {
-    const html = `<img data-index="${tIndex}" src="${bUrl}" width="100px" height="100px">`;
+  const buildImgIcon = function (tIndex, bUrl) {
+    const html = `<img data-indexIcon="${tIndex}" src="${bUrl}" width="100px" height="100px">`;
     return html;
   };
-  let fileIndex = [1, 2];
+  let fileIndexIcon = [1, 2];
 
-  lastIndex = $(".jsFileGroupIcon:last").data("index");
-  fileIndex.splice(0, lastIndex);
+  lastIndexIcon = $(".jsFileGroupIcon:last").data("indexIcon");
+  fileIndexIcon.splice(0, lastIndexIcon);
   $(".hiddenDestroyIcon").hide();
 
   // ==== 新規と編集 ================================
   $("#image-boxIcon").on("change", ".jsFileIcon", function (e) {
-    const targetIndex = $(this).parent().parent().data("index");
+    const targetIndex = $(this).parent().parent().data("indexIcon");
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
 
-    if ((img = $(`img[data-index="${targetIndex}"]`).get(0))) {
+    if ((img = $(`img[data-indexIcon="${targetIndex}"]`).get(0))) {
       img.setAttribute("src", blobUrl);
     } else {
-      $("#previewsIcon").append(buildImg(targetIndex, blobUrl));
-      $("#image-boxIcon").append(buildFileField(fileIndex[0]));
+      $("#previewsIcon").append(buildImgIcon(targetIndex, blobUrl));
+      // $("#image-boxIcon").append(buildFileFieldIcon(fileIndexIcon[0]));
 
-      fileIndex.shift();
-      fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+      fileIndexIcon.shift();
+      fileIndexIcon.push(fileIndexIcon[fileIndexIcon.length - 1] + 1);
     }
   });
 
   // ==== 削除 =========================================
   $("#image-boxIcon").on("click", ".jsRemoveIcon", function () {
-    const targetIndex = $(this).parent().parent().data("index");
+    const targetIndex = $(this).parent().parent().data("indexIcon");
     const hiddenCheck = $(
-      `input[data-index="${targetIndex}"].hiddenDestroyIcon`
+      `input[data-indexIcon="${targetIndex}"].hiddenDestroyIcon`
     );
     if (hiddenCheck) hiddenCheck.prop("checked", true);
 
-    $(`img[data-index="${targetIndex}"]`).remove();
+    $(`img[data-indexIcon="${targetIndex}"]`).remove();
     $(this).parent().parent().remove();
 
     if ($(".jsFileIcon").length == 0)
-      $("#image-boxIcon").append(buildFileField(fileIndex[0]));
+      $("#image-boxIcon").append(buildFileFieldIcon(fileIndexIcon[0]));
   });
 });
