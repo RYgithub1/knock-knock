56e8c8dd8e53ce61f65e0fbee130cd6ca82b4eb5
RYgithub1 <ryo.rec.otani@gmail.com>
2020-06-24 11:01:17 +0900

    add_lastDate_comment_image_link

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -7,19 +7,4 @@ class Pair < ApplicationRecord
   has_many :messages, dependent: :destroy
   accepts_nested_attributes_for :messages, allow_destroy: true
 
-
-
-  # ラストメッセージの表示＠一覧表示
-  # def show_last_message
-  #   if (last_message = message.last).present?
-  #     if last_message.content?
-  #       last_message.content
-  #     else
-  #       "last one is the pucture"
-  #     end
-  #   else
-  #     "no message yet"
-  #   end
-  # end
-
 end
