95a63e9b7ab9a6a960153ce72a84548b45977725
RYgithub1 <ryo.rec.otani@gmail.com>
2020-07-17 13:26:47 +0900

    fix_databaseYml

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,28 +1,28 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+# サーバ上でのアプリケーションコードが設置されているディレクトリを変数に代入
 app_path = File.expand_path('../../', __FILE__)
 
-#アプリケーションサーバの性能を決定する（リクエストを受け付けレスポンスを生成するworker(ワーカー)の数を決定）
+# Appサーバの性能を決定（リクエストを受け付けレスポンスを生成するworker(ワーカー)の数を決定）
 worker_processes 1
 
-#UnicornがRailsのコードを動かす際、アプリケーションの設置されているディレクトリを指定
+# UnicornがRailsのコードを動かす際、アプリケーションの設置されているディレクトリを指定
 working_directory app_path
 
-#Unicornの起動に必要なファイルの設置場所を指定（起動する際にプロセスidが書かれたファイルを生成）
+# Unicornの起動に必要なファイルの設置場所を指定（起動する際にプロセスidが書かれたファイルを生成）
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
-#ポート番号を指定（どのポート番号のリクエストを受け付けるか）
+# port番号（どのポート番号のリクエストを受け付けるか）
 listen 3000
 
-#エラーのログを記録するファイルを指定(-> $less log/unicorn.stderr.log  ,or $tail)
+# [エラーログ]の格納ファイル(-> $less log/unicorn.stderr.log  ,or $tail)
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 
-#通常のログを記録するファイルを指定
+# [通常ログ]の格納ファイル
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
-#Railsアプリケーションの応答を待つ上限時間を設定
-timeout 60
 
-#以下は応用的な設定なので説明は割愛
+
+# Railsアプリの応答待ち上限時間
+timeout 60
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
@@ -36,7 +36,7 @@ before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect!
 
   if run_once
-    run_once = false # prevent from firing again
+    run_once = false  # prevent from firing again
   end
 
   old_pid = "#{server.config[:pid]}.oldbin"
