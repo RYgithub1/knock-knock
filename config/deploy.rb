6fda6038366641b72efb2f4f60b3d08c5c1464ea
RYgithub1 <ryo.rec.otani@gmail.com>
2020-07-22 10:38:03 +0900

    set_capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+# config valid only for current version of Capistrano
+# version of capistrano. 固定のバージョンを利用し続け、バージョン変更によるトラブルを防止
+lock '3.14.1'
+
+# Capistranoのログの表示に利用
+set :application, 'knock-knock'
+
+# アプリを自動でpullするRepositoryを指定
+set :repo_url,  'git@github.com:RYgithub1/knock-knock.git'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+# 公開鍵指定
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/knock.pem']
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理後、Unicornを再起動するため記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,38 +1,31 @@
 # サーバ上でのアプリケーションコードが設置されているディレクトリを変数に代入
-app_path = File.expand_path('../../', __FILE__)
-# app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
 
 # Appサーバの性能を決定（リクエストを受け付けレスポンスを生成するworker(ワーカー)の数を決定）
 worker_processes 1
-# working_directory "#{app_path}/current"
 
 # UnicornがRailsのコードを動かす際、アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+working_directory "#{app_path}/current"
+
 
 # Unicornの起動に必要なファイルの設置場所を指定（起動する際にプロセスidが書かれたファイルを生成）
-pid "#{app_path}/tmp/pids/unicorn.pid"
-# pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
 
 # port番号（どのポート番号のリクエストを受け付けるか）
-listen "#{app_path}/tmp/sockets/unicorn.sock"
-# listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
 
 # [エラーログ]の格納ファイル(-> $less log/unicorn.stderr.log  ,or $tail)
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-# stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
 
 # [通常ログ]の格納ファイル
-stdout_path "#{app_path}/log/unicorn.stdout.log"
-# stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
-
-
-
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 # Railsアプリの応答待ち上限時間
 timeout 60
+
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
