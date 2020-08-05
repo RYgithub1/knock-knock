12b0883258a7e5c17c3cdadc8a6061adb2dd7899
RYgithub1 <ryo.rec.otani@gmail.com>
2020-08-06 06:47:11 +0900

    fix_unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,75 @@
-# config valid only for current version of Capistrano
-# version of capistrano. 固定のバージョンを利用し続け、バージョン変更によるトラブルを防止
-lock '3.14.1'
+# # config valid only for current version of Capistrano
+# # version of capistrano. 固定のバージョンを利用し続け、バージョン変更によるトラブルを防止
+# lock '3.14.1'
 
-# Capistranoのログの表示に利用
-set :application, 'knock-knock'
+# # Capistranoのログの表示に利用
+# set :application, 'knock-knock'
 
-# アプリを自動でpullするRepositoryを指定
-set :repo_url, 'git@github.com:RYgithub1/knock-knock.git'
+# # アプリを自動でpullするRepositoryを指定
+# set :repo_url, 'git@github.com:RYgithub1/knock-knock.git'
 
-# バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# # バージョンが変わっても共通で参照するディレクトリを指定
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
+# set :rbenv_type, :user
+# set :rbenv_ruby, '2.5.1'
 
-# 公開鍵指定
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/knock.pem']
+# # 公開鍵指定
+# set :ssh_options, auth_methods: ['publickey'],
+#                   keys: ['~/.ssh/knock.pem']
 
-# プロセス番号を記載したファイルの場所
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+# # プロセス番号を記載したファイルの場所
+# set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Unicornの設定ファイルの場所
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
+# # Unicornの設定ファイルの場所
+# set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+# set :keep_releases, 5
 
-# デプロイ処理後、Unicornを再起動するため記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
+# # デプロイ処理後、Unicornを再起動するため記述
+# after 'deploy:publishing', 'deploy:restart'
+# namespace :deploy do
+#   task :restart do
+#     invoke 'unicorn:restart'
+#   end
+# end
+
+# --------
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.13.0"
+
+set :application, "my_app_name"
+set :repo_url, "git@example.com:me/my_repo.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '52.192.44.169', user: 'ec2-user', roles: %w{app db web}
+# server '52.192.44.169', user: 'ec2-user', roles: %w{app db web}
 
 # server-based syntax
 # ======================
